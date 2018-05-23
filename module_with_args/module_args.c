07d01e57936b96678c70ba5dd0e7a956ca696b89
Rishabh Hardas <rishabhhardas@gmail.com>
2018-05-23 22:13:06 +0530

    added module_param_named

--- module_with_args/module_args.c
+++ module_with_args/module_args.c
@@ -14,9 +14,24 @@
 
 short int sint_param = 0;
 
+/*
+* The file will show up as sint_param in sysfs
+*/
 module_param(sint_param, short, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
 MODULE_PARM_DESC(sint_param, "Simple short int argument");
 
+/*
+* To change the name of the sysfs file (sint_param)
+* we use the following macro
+*/
+module_param_named(param_file, sint_param, short, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+MODULE_PARM_DESC(param_file, "File name to appear in sysfs");
+
+/*
+* In this case both sint_param and param_file will be visible in sysfs
+* with the permissions defined above.
+* Values can be changed from sysfs
+*/
 int __init hello_init(void)
 {
 	printk(KERN_ALERT "Hello World!\n");
@@ -26,6 +41,7 @@ int __init hello_init(void)
 
 void __exit hello_exit(void)
 {
+	printk(KERN_ALERT"Short int = %d\n", sint_param);
 	printk(KERN_ALERT "Exit\n");
 }
 
